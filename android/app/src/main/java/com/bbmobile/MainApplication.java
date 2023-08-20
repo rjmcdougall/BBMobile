2141fdbea03c888c3a7ce0b35abaa3d3ef544110
Daniel Wilson <danielkeithw@gmail.com>
2023-08-20 12:40:15 -0700

    content resolver android fixes for monitor mode

--- android/app/src/main/java/com/bbmobile/MainApplication.java
+++ android/app/src/main/java/com/bbmobile/MainApplication.java
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
+          packages.add(new ContentResolverPackage());
           // packages.add(new MyReactNativePackage());
           return packages;
         }
@@ -88,4 +89,7 @@ public class MainApplication extends Application implements ReactApplication {
       }
     }
   }
+
+    public static class ContentResolverModule {
+    }
 }
