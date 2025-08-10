14b4b8786821240f7f31f019f1095b7e470bb1fd
Richard McDougall <git@3emus.com>
2025-08-09 23:34:54 -0700

    Move media controls onto map and make map the default screen

--- BoardManager.js
+++ BoardManager.js
@@ -50,7 +50,7 @@ export default class BoardManager extends Component {
 			appState: "",
 			connectedPeripheral: StateBuilder.blankPeripheral(),
 			boardState: StateBuilder.blankBoardState(),
-			showScreen: Constants.MEDIA_MANAGEMENT,
+			showScreen: Constants.MAP,
 			automaticallyConnect: true,
 			backgroundLoop: null,
 			rxBuffers: [],
@@ -405,7 +405,7 @@ export default class BoardManager extends Component {
 					video: StateBuilder.blankVideo(),
 					audio: StateBuilder.blankAudio(),
 					devices: StateBuilder.blankDevices(),
-					showScreen: Constants.MEDIA_MANAGEMENT,
+					showScreen: Constants.MAP,
 					boardName: boardName,
 					scanning: false,
 				});
@@ -885,11 +885,10 @@ export default class BoardManager extends Component {
 						{(!this.props.userPrefs.isDevilsHand) ? <LeftNav onNavigate={this.onNavigate} showScreen={this.state.showScreen} onPressSearchForBoards={this.onPressSearchForBoards} /> : <View></View>}
 						<View style={{ flex: 1 }}>
 							<View style={{ flex: 1 }}>
-								{(this.state.showScreen == Constants.MEDIA_MANAGEMENT) ? <MediaManagement pointerEvents={enableControls} audio={this.state.audio} video={this.state.video} boardState={this.state.boardState} sendCommand={this.sendCommand} /> : <View></View>}
 								{(this.state.showScreen == Constants.DIAGNOSTIC) ? <Diagnostic pointerEvents={enableControls} logLines={this.state.logLines} boardState={this.state.boardState} /> : <View></View>}
 								{(this.state.showScreen == Constants.ADMINISTRATION) ? <AdminManagement wifi={this.state.wifi} devices={this.state.devices} setUserPrefs={this.props.setUserPrefs} userPrefs={this.props.userPrefs} pointerEvents={enableControls} boardState={this.state.boardState} sendCommand={this.sendCommand} /> : <View></View>}
 								{(this.state.showScreen == Constants.APP_MANAGEMENT) ? <AppManagement updateMonitor={this.updateMonitor} clearCache={this.clearCache} setUserPrefs={this.props.setUserPrefs} userPrefs={this.props.userPrefs} /> : <View></View>}
-								{(this.state.showScreen == Constants.MAP) ? <MapController ref={ref => this.mapControllerRef = ref} isMonitor={this.state.isMonitor} updateMonitor = {this.updateMonitor} userPrefs={this.props.userPrefs} boardState={this.state.boardState} locations={this.state.locations} setMap={this.setMap} map={this.state.map} boardData={this.state.boardData} setUserPrefs={this.props.setUserPrefs} sendMessageToBLE={this.sendMessageToBLE.bind(this)} fetchMessages={this.fetchMessages.bind(this)} /> : <View></View>}
+								{(this.state.showScreen == Constants.MAP) ? <MapController ref={ref => this.mapControllerRef = ref} isMonitor={this.state.isMonitor} updateMonitor = {this.updateMonitor} userPrefs={this.props.userPrefs} boardState={this.state.boardState} locations={this.state.locations} setMap={this.setMap} map={this.state.map} boardData={this.state.boardData} setUserPrefs={this.props.setUserPrefs} sendMessageToBLE={this.sendMessageToBLE.bind(this)} fetchMessages={this.fetchMessages.bind(this)} audio={this.state.audio} video={this.state.video} sendCommand={this.sendCommand} /> : <View></View>}
 								{(this.state.showScreen == Constants.DISCOVER) ? <DiscoverController startScan={this.startScan} boardBleDevices={this.state.boardBleDevices} scanning={this.state.scanning} boardData={this.state.boardData} onSelectPeripheral={this.onSelectPeripheral} /> : <View></View>}
 								{(this.state.showScreen == Constants.STATS_CONTROL) ? <StatsControl pointerEvents={enableControls} boardState={this.state.boardState} sendCommand={this.sendCommand} /> : <View></View>}
 							</View>
--- LeftNav.js
+++ LeftNav.js
@@ -14,16 +14,8 @@ export default class LeftNav extends React.Component {
 
 	render() {
 		return (
-			<View style={{ width: 50, backgroundColor: "powderblue", margin: 2 }}>
-				<View style={[{ backgroundColor: this.props.showScreen == Constants.MEDIA_MANAGEMENT ? "green" : "lightblue" }]}>
-					<Touchable
-						onPress={() => {
-							this.props.onNavigate(Constants.MEDIA_MANAGEMENT);
-						}}
-						style={StyleSheet.icon}>
-						<MaterialCommunityIcon name="music-box-outline" size={40} color="black" />
-					</Touchable>
-				</View>
+			<View style={{ width: 50, backgroundColor: "powderblue", margin: 2, position: 'relative' }}>
+				{/* Top section - Main navigation */}
 				<View style={[{ backgroundColor: this.props.showScreen == Constants.MAP ? "green" : "lightblue" }]}>
 					<Touchable
 						onPress={() => {
@@ -42,24 +34,6 @@ export default class LeftNav extends React.Component {
 						<MaterialCommunityIcon name="cog" size={40} color="black" />
 					</Touchable>
 				</View>
-				<View style={[{ backgroundColor: this.props.showScreen == Constants.APP_MANAGEMENT  ? "green" : "lightblue" }]}>
-					<Touchable
-						onPress={() => {
-							this.props.onNavigate(Constants.APP_MANAGEMENT);
-						}}
-						style={StyleSheet.icon}>
-						<MaterialCommunityIcon name="cellphone" size={40} color="black" />
-					</Touchable>
-				</View>
-				<View style={[{ backgroundColor: this.props.showScreen == Constants.DIAGNOSTIC ? "green" : "lightblue" }]}>
-					<Touchable
-						onPress={() => {
-							this.props.onNavigate(Constants.DIAGNOSTIC);
-						}}
-						style={StyleSheet.icon}>
-						<MaterialCommunityIcon name="help-network" size={40} color="black" />
-					</Touchable>
-				</View>
 				<View style={[{ backgroundColor: this.props.showScreen == Constants.DISCOVER ? "green" : "lightblue" }]}>
 					<Touchable
 						onPress={ () => {
@@ -69,14 +43,36 @@ export default class LeftNav extends React.Component {
 						<MaterialCommunityIcon name="magnify" size={40} color="black" />
 					</Touchable>
 				</View>
-				<View style={[{ backgroundColor: this.props.showScreen == Constants.STATS_CONTROL ? "green" : "lightblue" }]}>
-					<Touchable
-						onPress={() => {
-							this.props.onNavigate(Constants.STATS_CONTROL);
-						}}
-						style={StyleSheet.icon}>
-						<MaterialCommunityIcon name="chart-bar" size={40} color="black" />
-					</Touchable>
+				
+				{/* Bottom section - Secondary/Admin functions - Absolutely positioned */}
+				<View style={{ position: 'absolute', bottom: 0, left: 0, width: 50 }}>
+					<View style={[{ backgroundColor: this.props.showScreen == Constants.APP_MANAGEMENT  ? "green" : "lightblue" }]}>
+						<Touchable
+							onPress={() => {
+								this.props.onNavigate(Constants.APP_MANAGEMENT);
+							}}
+							style={StyleSheet.icon}>
+							<MaterialCommunityIcon name="cellphone" size={40} color="black" />
+						</Touchable>
+					</View>
+					<View style={[{ backgroundColor: this.props.showScreen == Constants.DIAGNOSTIC ? "green" : "lightblue" }]}>
+						<Touchable
+							onPress={() => {
+								this.props.onNavigate(Constants.DIAGNOSTIC);
+							}}
+							style={StyleSheet.icon}>
+							<MaterialCommunityIcon name="help-network" size={40} color="black" />
+						</Touchable>
+					</View>
+					<View style={[{ backgroundColor: this.props.showScreen == Constants.STATS_CONTROL ? "green" : "lightblue" }]}>
+						<Touchable
+							onPress={() => {
+								this.props.onNavigate(Constants.STATS_CONTROL);
+							}}
+							style={StyleSheet.icon}>
+							<MaterialCommunityIcon name="chart-bar" size={40} color="black" />
+						</Touchable>
+					</View>
 				</View>
 			</View>
 		);
