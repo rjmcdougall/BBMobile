a44f721fda2af613eab7db580191c2f2b814a4c7
Richard McDougall <git@3emus.com>
2025-08-17 01:13:03 -0700

    Dark mode

--- Diagnostic.js
+++ Diagnostic.js
@@ -2,7 +2,7 @@ import React, { Component} from "react";
 import { View, ScrollView, Text,} from "react-native";
 import StateBuilder from "./StateBuilder";
 import PropTypes from "prop-types";
-import StyleSheet from "./StyleSheet";
+import StyleSheet, { Colors } from "./StyleSheet";
 export default class Diagnostic extends Component {
 	constructor(props) {
 		super(props);
@@ -13,18 +13,36 @@ export default class Diagnostic extends Component {
 			<View style={StyleSheet.container}  >
 
 				<ScrollView style={{ margin: 10 }}>
-					<Text>APK Version: {this.props.boardState.apkv} {"\n"}
+					<Text style={{ color: Colors.textPrimary, fontSize: 16, lineHeight: 24 }}>APK Version: {this.props.boardState.apkv} {"\n"}
 						Last Updated: {new Date(this.props.boardState.apkd).toDateString()} {"\n"}
 						SSID: {this.props.boardState.s} {"\n"}
 						IP Address: {this.props.boardState.ip} {"\n"}
 					</Text>
 					{
 						this.props.logLines.map((line) => {
-							var color = "white";
-							if (line.isError)
-								color = "yellow";
+							var textColor = Colors.textSecondary;
+							var backgroundColor = Colors.surfaceSecondary;
+							if (line.isError) {
+								textColor = Colors.accentError;
+								backgroundColor = Colors.surfaceTertiary;
+							}
 
-							return (<Text key={Math.random()} style={{ backgroundColor: color }}>{line.logLine}</Text>);
+							return (
+								<Text 
+									key={Math.random()} 
+									style={{ 
+										color: textColor, 
+										backgroundColor: backgroundColor,
+										padding: 4,
+										marginBottom: 2,
+										borderRadius: 4,
+										fontFamily: 'monospace',
+										fontSize: 12
+									}}
+								>
+									{line.logLine}
+								</Text>
+							);
 						})
 					}
 				</ScrollView>
